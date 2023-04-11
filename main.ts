17a11e4ea9ec00d3eb5351f25014985ca4658eec
SquirrelSneaky <130080197+SquirrelSneaky@users.noreply.github.com>
2023-04-11 11:41:21 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,75 @@
+/**
+ * 1 = Male
+ * 
+ * 2 = Female
+ */
+let Age = 0
+let Water_Wait = 0
+let Gender = 0
+basic.showString("A = Male B=Female")
+while (Gender == 0) {
+    if (input.buttonIsPressed(Button.A)) {
+        Gender = 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        Gender = 2
+    }
+}
 basic.forever(function () {
-    while (input.temperature() >= 15) {
+    if (input.temperature() >= 37) {
+        Water_Wait = 0
+    } else {
+        Water_Wait = 0
+    }
+})
+basic.forever(function () {
+    if (Age <= 70 && Gender == 1) {
         basic.showLeds(`
+            . . . . .
             . . . . #
-            # # # # #
-            . . . # .
             # . . # .
-            . # . # .
+            . # # . .
+            . . . . .
             `)
-        basic.pause(100)
-        basic.clearScreen()
+        if (Age <= 19 && Gender == 1) {
+            basic.showLeds(`
+                . # . . .
+                . # . . #
+                # . # # .
+                . # # . .
+                . . . . .
+                `)
+            if (Age <= 14 && Gender == 1) {
+                basic.showLeds(`
+                    . . . . #
+                    . . . . #
+                    # . . # .
+                    . # # # .
+                    . . . . #
+                    `)
+            }
+        }
+    }
+})
+/**
+ * Needs Work
+ */
+basic.forever(function () {
+    if (Gender == 1) {
+        basic.showString("Enter AGE")
+        while (input.buttonIsPressed(Button.A)) {
+            Age += 1
+            basic.showString("" + (Age))
+        }
+        while (input.buttonIsPressed(Button.B)) {
+            Age += -1
+            basic.showString("" + (Age))
+        }
+        while (input.buttonIsPressed(Button.AB)) {
+            basic.clearScreen()
+        }
+        basic.pause(5000)
+        basic.pause(5000)
+        basic.pause(5000)
     }
 })
