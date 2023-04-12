c996e52f80374ef26aefbd614408d77400801e63
SquirrelSneaky <130080197+SquirrelSneaky@users.noreply.github.com>
2023-04-12 10:18:43 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,37 @@
 /**
- * Needs Work
+ * 16 hours awake
+ */
+/**
+ * Ready
+ * 
+ * 1=no
+ * 
+ * 2=20+ male
+ * 
+ * 3=19-15 male
+ * 
+ * 4=14-9 male
+ * 
+ * 5=8- male
+ * 
+ * 6=20+ female
+ * 
+ * 7=19-15 female
+ * 
+ * 8=14-9 female
+ * 
+ * 9=8- female
  */
 /**
  * 1 = Male
  * 
  * 2 = Female
  */
-let Age = 0
 let Water_Wait = 0
+let Age = 0
 let Gender = 0
+let Ready = 1
+basic.showString("A = Male B=Female")
 while (Gender == 0) {
     if (input.buttonIsPressed(Button.A)) {
         Gender = 1
@@ -17,6 +40,27 @@ while (Gender == 0) {
         Gender = 2
     }
 }
+if (Gender == 1 || Gender == 2) {
+    for (let index = 0; index < 1; index++) {
+        basic.showString("Enter AGE. A=+ B=- A+B TO SUBMIT")
+    }
+}
+basic.forever(function () {
+    if (Gender == 1 || Gender == 2) {
+        if (!(input.buttonIsPressed(Button.AB))) {
+            if (input.buttonIsPressed(Button.A)) {
+                basic.clearScreen()
+                Age += 1
+                basic.showString("" + (Age))
+            }
+            if (input.buttonIsPressed(Button.B)) {
+                basic.clearScreen()
+                Age += -1
+                basic.showString("" + (Age))
+            }
+        }
+    }
+})
 basic.forever(function () {
     if (input.temperature() >= 37) {
         Water_Wait = 0
@@ -26,48 +70,57 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (Gender == 1 || Gender == 2) {
-        if (input.buttonIsPressed(Button.A)) {
-            basic.clearScreen()
-            Age += 1
-            basic.showString("" + (Age))
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            basic.clearScreen()
-            Age += -1
-            basic.showString("" + (Age))
-        }
-        while (input.buttonIsPressed(Button.AB)) {
-            basic.clearScreen()
-            if (Age <= 70 && Gender == 1) {
-                basic.showLeds(`
-                    . . . . .
-                    . . . . #
-                    # . . # .
-                    . # # . .
-                    . . . . .
-                    `)
-                if (Age <= 19 && Gender == 1) {
-                    basic.showLeds(`
-                        . # . . .
-                        . # . . #
-                        # . # # .
-                        . # # . .
-                        . . . . .
-                        `)
-                    if (Age <= 14 && Gender == 1) {
-                        basic.showLeds(`
-                            . . . . #
-                            . . . . #
-                            # . . # .
-                            . # # # .
-                            . . . . #
-                            `)
+        if (input.buttonIsPressed(Button.AB)) {
+            if (Age >= 20 && Gender == 1) {
+                Ready = 2
+                basic.showString("Age Set")
+            }
+            if (Age <= 19 && Gender == 1) {
+                if (Age >= 15) {
+                    Ready = 3
+                    basic.showString("Age Set")
+                }
+                if (Age <= 14 && Gender == 1) {
+                    if (Age >= 9) {
+                        Ready = 4
+                        basic.showString("Age Set")
                     }
+                }
+                if (Age <= 8 && Gender == 1) {
+                    Ready = 5
+                    basic.showString("Age Set")
+                }
+            }
+            if (Age >= 20 && Gender == 2) {
+                Ready = 6
+                basic.showString("Age Set")
+            }
+            if (Age <= 19 && Gender == 2) {
+                if (Age >= 15) {
+                    Ready = 7
+                    basic.showString("Age Set")
+                }
+                if (Age <= 14 && Gender == 2) {
+                    if (Age >= 9) {
+                        Ready = 8
+                        basic.showString("Age Set")
+                    }
+                }
+                if (Age <= 8 && Gender == 2) {
+                    Ready = 9
+                    basic.showString("Age Set")
                 }
             }
         }
     }
 })
 basic.forever(function () {
-	
+    if (Ready == 2) {
+        for (let index = 0; index < 1080; index++) {
+            basic.pause(5000)
+        }
+        music.playMelody("C5 F G C F E A C ", 203)
+        music.playMelody("C5 F G C F E A C ", 203)
+        basic.showString("Drink 1 cup of water")
+    }
 })
